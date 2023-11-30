14d643d5cb6a1d0cb0101fd834858244d6395a44
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-11-30 15:53:58 +0700

    fix init

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-const express = require('express');
-const app = express();
+require("dotenv/config");
+
+const { Server } = require("socket.io");
+
+const io = new Server({ cors: process.env.FRONTEND_HOST });
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+    socket.on("addNewUser", (userId) => {
+        !onlineUsers.some((ou) => ou.userId === userId) &&
+            onlineUsers.push({
+                userId,
+                socketId: socket.id
+            });
+
+        io.emit("getOnlineUsers", onlineUsers);
+    });
+
+    socket.on("sendMessage", (message) => {
+        const user = onlineUsers.find((u) => u.userId === message.recipientId);
+
+        if (user) {
+            io.to(user.socketId).emit("getMessage", message);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter((u) => u.socketId !== socket.id);
+
+        io.emit("getOnlineUsers", onlineUsers);
+    });
+});
+
+const port = 5000;
+io.listen(port);
