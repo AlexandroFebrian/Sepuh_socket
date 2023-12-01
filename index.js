cad338a196a3a093c9f84052d35cfb51148325a0
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-12-01 11:35:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,26 +7,27 @@ const io = new Server({ cors: process.env.FRONTEND_HOST });
 let onlineUsers = [];
 
 io.on("connection", (socket) => {
-    socket.on("addNewUser", (userId) => {
-        !onlineUsers.some((ou) => ou.userId === userId) &&
+    socket.on("addNewUser", (user_id) => {
+        !onlineUsers.some((ou) => ou.user_id === user_id) &&
             onlineUsers.push({
-                userId,
-                socketId: socket.id
+                user_id,
+                socket_id: socket.id
             });
 
         io.emit("getOnlineUsers", onlineUsers);
     });
 
-    socket.on("sendMessage", (message) => {
-        const user = onlineUsers.find((u) => u.userId === message.recipientId);
+    socket.on("sendMessage", (data) => {
+        const user = onlineUsers.find((u) => u.user_id === data.receiver_id);
 
         if (user) {
-            io.to(user.socketId).emit("getMessage", message);
+            io.to(user.socket_id).emit("getMessage", data); 
+            // data isinya message, chat_id, dan receiver_id
         }
     });
 
     socket.on("disconnect", () => {
-        onlineUsers = onlineUsers.filter((u) => u.socketId !== socket.id);
+        onlineUsers = onlineUsers.filter((u) => u.socket_id !== socket.id);
 
         io.emit("getOnlineUsers", onlineUsers);
     });
